7b7b3b4577f42467647bebd3e40285cbe183ea24
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-03-11 22:25:39 -0500

    I added 3 more questions, I made the quiz container to only display after clicking the start btn. the wrong answer penalize by substracting 10 seconds. pop message with the right or wrong answer is working.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,18 +1,16 @@
 //Declared variables
-var quizText = document.getElementById("quiz");
+var quizDiv = document.getElementById("quiz");
 var startBtn = document.getElementById("startBtn");
-var quizBox = document.getElementById("questionsBox");
 var resultsEl = document.getElementById("result");
 var questionsEl = document.getElementById("questions");
 var quizTimer = document.getElementById("timer");
 var introPage = document.getElementById("introBox");
 
-
+//Variables for the questions choices
 var buttonA = document.getElementById("a");
 var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
-
 
 // variables for the Quiz questions 
 var quizQuestions = [{
@@ -23,26 +21,48 @@ var quizQuestions = [{
     choiceD: "d) numbers",
     rightAnswer: "c"},
     
-   {question: "2) What does DOM stand for?",
+  {
+    question: "2) What does DOM stand for?",
     choiceA: "Document Object Model",
     choiceB: "Display Object Management",
     choiceC: "Digital Ordinance Model",
     choiceD: "Desktop Oriented Mode",
-    correctAnswer: "a"},
+    rightAnswer: "a"},
+   {
+    question: "3) What HTML tags are JavaScript code wrapped in?",
+    choiceA: "&lt;div&gt;",
+    choiceB: "&lt;link&gt;",
+    choiceC: "&lt;head&gt;",
+    choiceD: "&lt;script&gt;",
+    correctAnswer: "d"},
+  {
+    question: "4) What does WWW stand for?",
+    choiceA: "Web World Workings",
+    choiceB: "Weak Winter Wind",
+    choiceC: "World Wide Web",
+    choiceD: "Wendy Wants Waffles",
+    correctAnswer: "c"},
 ];
 
 
 var finalQuestionIndex = quizQuestions.length;
 var currentQuestionIndex = 0;
-var timeLeft = 76;
+//Timer 76 seconds
+var timeLeft = 80;
 var timerInterval;
 var score = 0;
 var correct;
 
-//This function generates the quiz questions and answers
-function generateQuizQuestion(){
+//Holds penalty time
+var timePenalty = 10;
 
-    if (currentQuestionIndex === finalQuestionIndex){
+  //Hides the quiz Div
+  quizDiv.style.display = "none";
+
+//This function generates the quiz questions and answers
+function generateQuizQuestion() {
+  
+  if (currentQuestionIndex === finalQuestionIndex) {
   
     } 
     var currentQuestion = quizQuestions[currentQuestionIndex];
@@ -51,28 +71,62 @@ function generateQuizQuestion(){
     buttonB.innerHTML = currentQuestion.choiceB;
     buttonC.innerHTML = currentQuestion.choiceC;
     buttonD.innerHTML = currentQuestion.choiceD;
-}
+  
+  //displays the quiz div
+  quizDiv.style.display = "block";
 
+   }
+
+ 
 //This function starts the timer, hides the infoBox, & displays the first question
-function startQuiz(){
-
-    introPage.style.display = "none"; 
-    generateQuizQuestion();
-
-    //Timer
-    timerInterval = setInterval(function() {
-        timeLeft--;
-        quizTimer.textContent = "Time left: " + timeLeft;
+function startQuiz() {
+  //It hides the info box   
+  introPage.style.display = "none";
+  generateQuizQuestion();
+  //Timer
+  timerInterval = setInterval(function () {
+    timeLeft--;
+    quizTimer.textContent = "Time left: " + timeLeft;
     
-        if(timeLeft === 0) {
-          clearInterval(timerInterval);
-          showScore();
-        }
-      }, 1000);
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+      // showScore();
+      quizTimer.textContent = "Time's up!";
+
+      }
+    
+  }, 1000);
 }
 
 
+//This function checks the response for each answer 
+function checkAnswer(answer){
+    correct = quizQuestions[currentQuestionIndex].rightAnswer;
 
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+        score++;
+        alert("Correct Answer!");
+        currentQuestionIndex++;
+      generateQuizQuestion();
+      
+     //I need to add the right answer to the score 
+    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+      alert("Wrong Answer!")
+      //time penalty of 10 sec for each wrong answer
+      timeLeft = timeLeft - timePenalty;
+      currentQuestionIndex++;
+      generateQuizQuestion();
+        
+    }
+    //display in the results div that the answer is wrong.
+    // else {
+    //     showScore();
+    // }
+}
+
+// function resetAnswers
+
+ 
 //Event listener to the start button 
 startBtn.addEventListener("click", startQuiz);
 
