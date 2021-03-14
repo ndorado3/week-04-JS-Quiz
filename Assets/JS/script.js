cdbd6dcefe0d625ac4c01a8f6b7f720a298096e7
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-03-13 23:48:07 -0500

    I added the final score display and initials input.The submit button displays the highscore history.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -6,6 +6,14 @@ var questionsEl = document.getElementById("questions");
 var quizTimer = document.getElementById("timer");
 var introPage = document.getElementById("introBox");
 var gameOverDiv = document.getElementById("gameOver");
+var finalScoreEl = document.getElementById("finalScore");
+var highscoreDiv = document.getElementById("highScoreCard");
+var highScoreContainer = document.getElementById("highScoreContainer");
+var submitScoreBtn = document.getElementById("submitScorebtn");
+var highScoreDiv = document.getElementById("highScoreCard");
+var initials = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var highscoreDisplayScore = document.getElementById("highscore-score");
 
 //Variables for the questions choices
 var buttonA = document.getElementById("a");
@@ -26,33 +34,32 @@ var quizQuestions = [
 
   {
     question: "2) What does DOM stand for?",
-    choiceA: "Document Object Model",
-    choiceB: "Display Object Management",
-    choiceC: "Digital Ordinance Model",
-    choiceD: "Desktop Oriented Mode",
+    choiceA: "a) Document Object Model",
+    choiceB: "b) Display Object Management",
+    choiceC: "c) Digital Ordinance Model",
+    choiceD: "d) Desktop Oriented Mode",
     rightAnswer: "a",
   },
   {
     question: "3) What HTML tags are JavaScript code wrapped in?",
-    choiceA: "&lt;div&gt;",
-    choiceB: "&lt;link&gt;",
-    choiceC: "&lt;head&gt;",
-    choiceD: "&lt;script&gt;",
+    choiceA: "a) &lt;div&gt;",
+    choiceB: "b) &lt;link&gt;",
+    choiceC: "c) &lt;head&gt;",
+    choiceD: "d) &lt;script&gt;",
     correctAnswer: "d",
   },
   {
     question: "4) What does WWW stand for?",
-    choiceA: "Web World Workings",
-    choiceB: "Weak Winter Wind",
-    choiceC: "World Wide Web",
-    choiceD: "Wendy Wants Waffles",
+    choiceA: "a) Web World Workings",
+    choiceB: "b) Weak Winter Wind",
+    choiceC: "c) World Wide Web",
+    choiceD: "d) Wendy Wants Waffles",
     correctAnswer: "c",
   },
 ];
 
 var finalQuestionIndex = quizQuestions.length;
 var currentQuestionIndex = 0;
-//Timer 76 seconds
 var timeLeft = 80;
 var timerInterval;
 var score = 0;
@@ -61,8 +68,9 @@ var correct;
 //Holds penalty time
 var timePenalty = 10;
 
-//Hides the quiz Div
+//Hides the quiz Div & high score div
 quizDiv.style.display = "none";
+highScoreContainer.style.display = "none";
 
 //This function generates the quiz questions and answers
 function generateQuizQuestion() {
@@ -88,13 +96,12 @@ function startQuiz() {
   timerInterval = setInterval(function () {
     timeLeft--;
     quizTimer.textContent = "Time left: " + timeLeft;
-    //
+    //this if statemnt end the quiz when the timer=0
     if (timeLeft <= 0) {
       //call the gameover function
       gameOver();
-      //hide the quiz section 
-    quizDiv.style.display = "none"; 
-      // showScore();
+      //hide the quiz section
+      quizDiv.style.display = "none";
     }
   }, 1000);
 }
@@ -103,54 +110,103 @@ function startQuiz() {
 function checkAnswer(answer) {
   correct = quizQuestions[currentQuestionIndex].rightAnswer;
   console.log(correct, answer, currentQuestionIndex);
-  
+
   if (answer === correct) {
     score++;
     alert("Correct Answer!");
-    //I need to add the right answer to the score
   } else {
     alert("Wrong Answer!");
     //time penalty of 10 sec for each wrong answer
     timeLeft = timeLeft - timePenalty;
   }
+
   //goes to the next question
   currentQuestionIndex++;
-
-//
+  //this is statemant ends the quiz & takes the user to the final score block
   if (currentQuestionIndex === quizQuestions.length) {
     gameOver();
-    // quizDiv.classList.add("hide");
-    //hide the quiz  
-    quizDiv.style.display = "none"; 
+    //hides the quiz
+    quizDiv.style.display = "none";
   } else {
     generateQuizQuestion();
   }
-  //display in the results div that the answer is wrong.
-  // else {
-  //     showScore();
-  // }
 }
 
-// Validates the initials in at the game over form
-function validateForm() {
-  var x = document.forms["scoreForm"]["initials"].value;
-  if (x === "") {
-    alert("Your initials must be enter");
-    return false;
-  }
-}
-
-//game over function
+//This function displays the final score = time remainig
 function gameOver() {
   quizTimer.textContent = "Time's up!";
-  clearInterval(timerInterval);
-  
   gameOverDiv.classList.remove("hide");
+  clearInterval(timerInterval);
+  if (timeLeft >= 0) {
+    finalScoreEl.innerHTML = "";
+    var timeRemaining = timeLeft;
+    finalScoreEl.textContent = "Your final score is: " + timeRemaining;
+    quizDiv.style.display = "none";
+  }
 
-  //unhide div gmae over div
-  //hide quiz section
 
-  //set final score = timeleft
+
+
+
+  submitScoreBtn.addEventListener("click", highScore);
+
+  function highScore() {
+    if (initials.value === "") {
+      alert("Must input your initials!");
+      return;
+    } else {
+      var savedHighscores =
+        JSON.parse(localStorage.getItem("savedHighscores")) || [];
+      var currentUser = initials.value.trim();
+      var currentHighscore = {
+        name: currentUser,
+        score: score
+      };
+
+      gameOverDiv.style.display = "none";
+      highScoreContainer.style.display = "block";
+
+      savedHighscores.push(currentHighscore);
+      localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+      generateHighscores();
+    }
+  }
+
+  function generateHighscores() {
+    highscoreDisplayName.innerHTML = "";
+    highscoreDisplayScore.innerHTML = "";
+
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i = 0; i < highscores.length; i++) {
+      var newNameSpan = document.createElement("li");
+      var newScoreSpan = document.createElement("li");
+      newNameSpan.textContent = highscores[i].name;
+      newScoreSpan.textContent = highscores[i].score;
+      highscoreDisplayName.appendChild(newNameSpan);
+      highscoreDisplayScore.appendChild(newScoreSpan);
+    }
+  }
+  function scoreClear(){
+    window.localStorage.clear();
+    // highscoreDisplayName.textContent = "";
+    // highscoreDisplayScore.textContent = "";
+}
+function showHighscore() {
+  startQuizDiv.style.display = "none"
+  gameoverDiv.style.display = "none";
+  highscoreContainer.style.display = "flex";
+  generateHighscores();
+
+}
+function replayQuiz(){
+  highscoreContainer.style.display = "none";
+  gameoverDiv.style.display = "none";
+  startQuizDiv.style.display = "flex";
+  timeLeft = 78;
+  score = 0;
+  currentQuestionIndex = 0;
+}
+
 }
 
 //Event listener to the start button
--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # My Awsome JS Quiz 
+This project is an assigned homework for the Georgia Tech coding Bootcamp. I created the HTL,CSS and Javascript code.
 
+This is a quiz application using HTML, CSS, and Javascript. This application emphasizes the use of Javascript to provide quiz questions and collect user data to determine whether the answers to a question are correct, this then generates a score and appends a final page of results from the user data.
 
+## Code Critirias
 
-## Task
-
-
+1. By clicking the Start button the quiz begins and triggers the timer.
+2. The first questions is display.
+3. When the current question is answer it diplays the next question. 
+4. When the user answered a question wrong the he/she is penalized by subtracting 10 seconds from the clock/timer.
+5. The quiz is over, when all the questions are answered or the time reaches 0.
+6. The user can save he/she initials and scores. 
+7. The user can retake the quiz and clear the score history. 
 
 ## User Story
 
@@ -14,4 +21,21 @@ I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
 SO THAT I can gauge my progress compared to my peers
 ```
 
+## Visuals
 
+The following is a screenshot of the quiz's wbb page:
+
+Quiz information 
+
+![This is a Javascript quiz".](./assets/img/main_pg.png)
+
+Quiz question 
+
+![Question format".](./assets/img/questions.png)
+
+
+## Link
+
+The following is the live link of the application:
+
+    https://ndorado3.github.io/week-04-JS-Quiz/
